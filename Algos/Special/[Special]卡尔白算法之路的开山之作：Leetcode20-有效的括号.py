c7b12c366ea77492542d1a072063cbb58eca08bb
Karlbaey101 <jerrychen101@outlook.com>
2025-04-19 14:13:10 +0800

    整合题面描述

--- Algos/Special/[Special]卡尔白算法之路的开山之作：Leetcode20-有效的括号.py
+++ Algos/Special/[Special]卡尔白算法之路的开山之作：Leetcode20-有效的括号.py
@@ -1,33 +1,4 @@
-"""
-题面描述：
-给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
-
-有效字符串需满足：
-
-1. 左括号必须用相同类型的右括号闭合。
-2. 左括号必须以正确的顺序闭合。
-3. 每个右括号都有一个对应的相同类型的左括号。
-
-示例 1：
-    I: s = "()"
-    O: True
-
-示例 2：
-    I: s = "()[]{}"
-    O: True
-
-示例 3：
-    I: s = "([)]"
-    O: False
-
-示例 4：
-    I: s = "(]"
-    O: False
-
-示例 5：
-    I: s = "([{}])"
-    O: True
-"""
+# 题面描述：https://leetcode.cn/problems/valid-parentheses/description/
 
 class Solution:
     def isValid(self,s: str) -> bool:
