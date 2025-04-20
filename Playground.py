6a24d86eefaaebcca9dc43d23bd45c529f1a647b
Karlbaey101 <jerrychen101@outlook.com>
2025-04-20 18:18:36 +0800

    一个二叉树！

--- Playground.py
+++ Playground.py
@@ -1,4 +1,4 @@
-s = "00111"
+s = "1110"
 """
 0|0111
 """
@@ -14,3 +14,16 @@ for i in s:
         print("Right score is",r)
     ans = max(ans,l+r)
 print(ans,r+l)
+
+# 这有个二叉树！
+class TreeNode:
+    def __init__(self,data):
+        self.data = data # 给节点数据
+        self.left = None # 左节点
+        self.right = None # 右节点
+
+tree = TreeNode(10)
+tree.left = TreeNode(4)
+tree.right = TreeNode(6)
+
+print(tree.data == tree.left.data + tree.right.data)
